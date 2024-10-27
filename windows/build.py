3cea340f97afd7534c0663ddf5a745491ba6a835
nyne <nyne19710@proton.me>
2024-10-27 20:16:04 +0800

    build scripts & issue templates

---
+++ windows/build.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+
+file = open('pubspec.yaml', 'r')
+content = file.read()
+file.close()
+
+subprocess.run(["flutter", "build", "windows"], shell=True)
+
+if os.path.exists("build/app-windows.zip"):
+    os.remove("build/app-windows.zip")
+
+version = str.split(str.split(content, 'version: ')[1], '+')[0]
+
+subprocess.run(["tar", "-a", "-c", "-f", f"build/windows/Venera-{version}-windows.zip", "-C", "build/windows/x64/runner/Release", "."]
+               , shell=True)
+
+issContent = ""
+file = open('windows/build.iss', 'r')
+issContent = file.read()
+newContent = issContent
+newContent = newContent.replace("{{version}}", version)
+newContent = newContent.replace("{{root_path}}", os.getcwd())
+file.close()
+file = open('windows/build.iss', 'w')
+file.write(newContent)
+file.close()
+
+subprocess.run(["iscc", "windows/build.iss"], shell=True)
+
+with open('windows/build.iss', 'w') as file:
+    file.write(issContent)
