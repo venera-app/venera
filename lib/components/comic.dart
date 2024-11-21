265d5447eee61d4094ec1056753d58815a2e7d14
pkuislm <69719051+pkuislm@users.noreply.github.com>
2024-11-22 04:30:05 +0800

    use openFilePlatform

--- lib/components/comic.dart
+++ lib/components/comic.dart
@@ -154,8 +154,6 @@ class ComicTile extends StatelessWidget {
     ImageProvider image;
     if (comic is LocalComic) {
       image = FileImage((comic as LocalComic).coverFile);
-    } else if (comic.cover.startsWith('file://')) {
-      image = FileImage(File(comic.cover.substring(7)));
     } else if (comic.sourceKey == 'local') {
       var localComic = LocalManager().find(comic.id, ComicType.local);
       if (localComic == null) {
