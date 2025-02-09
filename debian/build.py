8a40a7b98c2924eb22a1c45d7486a34c98a659f0
nyne <me@nyne.dev>
2025-02-09 14:15:16 +0800

    Add linux-arm64 workflow

--- debian/build.py
+++ debian/build.py
@@ -1,5 +1,7 @@
 import subprocess
+import sys
 
+arch = sys.argv[1]
 debianContent = ''
 desktopContent = ''
 version = ''
@@ -12,7 +14,14 @@ with open('pubspec.yaml', 'r') as f:
     version = str.split(str.split(f.read(), 'version: ')[1], '+')[0]
 
 with open('debian/debian.yaml', 'w') as f:
-    f.write(debianContent.replace('{{Version}}', version))
+    content = debianContent.replace('{{Version}}', version)
+    if arch == 'x64':
+        content = content.replace('{{Arch}}', 'x64')
+        content = content.replace('{{Architecture}}', 'amd64')
+    elif arch == 'arm64':
+        content = content.replace('{{Arch}}', 'arm64')
+        content = content.replace('{{Architecture}}', 'arm64')
+    f.write(content)
 with open('debian/gui/venera.desktop', 'w') as f:
     f.write(desktopContent.replace('{{Version}}', version))
 
