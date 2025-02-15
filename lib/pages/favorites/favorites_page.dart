198966920ef810333e533a40533da57926f65b01
nyne <me@nyne.dev>
2025-02-15 16:08:15 +0800

    Add "Copy Title" to local favorites page.

--- lib/pages/favorites/favorites_page.dart
+++ lib/pages/favorites/favorites_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_reorderable_grid_view/widgets/reorderable_builder.dart';
 import 'package:venera/components/components.dart';
 import 'package:venera/foundation/app.dart';
