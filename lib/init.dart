23ee79fe9d604705f03364883fdc151f2f231c7d
nyne <me@nyne.dev>
2025-06-23 19:39:47 +0800

    Set high refresh rate on Android.

--- lib/init.dart
+++ lib/init.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:display_mode/display_mode.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_saf/flutter_saf.dart';
@@ -50,6 +51,11 @@ Future<void> init() async {
   if (App.isAndroid) {
     handleLinks();
     handleTextShare();
+    try {
+      await FlutterDisplayMode.setHighRefreshRate();
+    } catch(e) {
+      Log.error("Display Mode", "Failed to set high refresh rate: $e");
+    }
   }
   FlutterError.onError = (details) {
     Log.error("Unhandled Exception", "${details.exception}\n${details.stack}");
